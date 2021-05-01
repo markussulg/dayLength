9dd14346ceb5f01100d2ec0a3dc0ae25ac6266ef
markus sulg <markus7sulg@gmail.com>
2021-05-01 23:42:34 +0300

    Initial commit

---
+++ src/test/java/com/internship/dayLength/DayLengthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.dayLength;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayLengthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
