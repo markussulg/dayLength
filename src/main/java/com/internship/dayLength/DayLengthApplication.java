9234c7b541b075d2878fab72807f2344a35d20c1
markus sulg <markus7sulg@gmail.com>
2021-05-02 01:08:59 +0300

    GET and POST requests work

--- src/main/java/com/internship/dayLength/DayLengthApplication.java
+++ src/main/java/com/internship/dayLength/DayLengthApplication.java
@@ -2,20 +2,29 @@ package com.internship.dayLength;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class DayLengthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DayLengthApplication.class, args);
 	}
-
 	@GetMapping
-	public String hello() {
-		return "hello, world!";
+	public String showForm() {
+
+		return "mainView";
+	}
+	@PostMapping
+	public String submitCalculation(@ModelAttribute("lat") String lat, @ModelAttribute("long") String longitude,
+									@ModelAttribute("time") String time) {
+		System.out.println(lat + longitude + time);
+		return "mainView";
 	}
 
 }
